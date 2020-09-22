83f4c068e17d4177ccf0bf94b7dcdfdc2629b9a8
Clarice <clari.groeneveld@gmail.com>
2020-09-22 14:10:57 +0200

    fix celldiv_moment exception

--- scycle/tools/_celldiv_moment.py
+++ scycle/tools/_celldiv_moment.py
@@ -26,12 +26,15 @@ def celldiv_moment (adata: AnnData, verbose: bool= True):
     #-- Find moment of division (max abs diff)
     edge_to_max = np.argmax(edges['dif_counts'])
     sugg_edge = edges.iloc[edge_to_max][['e1', 'e2']].values.astype(int)
-      
-    #-- Find direction of cell division
-    x = np.array(edges['e1']).reshape((-1,1))
-    y = np.array(edges['mean_counts'])
-    model = LinearRegression().fit(x, y)
-    direction = 1 if (model.coef_ > 0)[0] else -1
+    
+    if sugg_edge[0] != 0:
+        coef1 = _edgeRegCoefficient(edges, end_edge = sugg_edge[0])
+        coef2 = _edgeRegCoefficient(edges, start_edge = sugg_edge[0])
+        
+        direction = -1 if (coef1 < 0)[0] & (coef2 < 0)[0] else 1
+    else:
+        coef = _edgeRegCoefficient(edges)
+        direction = 1 if (coef > 0)[0] else -1
     
     if verbose: 
         print('Suggested moment of cell division:', sugg_edge)
@@ -40,3 +43,16 @@ def celldiv_moment (adata: AnnData, verbose: bool= True):
     #-- Add data to object
     adata.uns['scycle']['cell_div_moment'] = {'cell_div_edge': sugg_edge, 
                                               'cell_cycle_direction': direction}
+    
+def _edgeRegCoefficient(edges, start_edge = 0, end_edge = None):
+    if end_edge is None:
+        end_edge = len(edges)
+    idx = (edges['e1'] < end_edge) & (edges['e1'] >= start_edge)
+    
+    x = np.array(edges['e1'][idx]).reshape((-1,1))
+    y = np.array(edges['mean_counts'][idx])
+    model = LinearRegression().fit(x, y)
+    
+    return model.coef_
+    
+    
