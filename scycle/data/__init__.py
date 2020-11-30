003419f704ff595e648931b3ab0effb973fb7e3c
Aziz Fouché <afouche@ens-paris-saclay.fr>
2020-11-30 17:27:03 +0100

    enrichment data

---
+++ scycle/data/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+#
+from ._cc_markers import (
+    freeman_G1S,
+    freeman_G2M,
+    g2m_markers,
+    g1s_markers,
+    g2m_inhibitory_markers,
+    histone_markers,
+)
