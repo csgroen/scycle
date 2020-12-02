24dcdbb9203f2b8ab63fd3610e6a66bf6d062c8b
Clarice <clari.groeneveld@gmail.com>
2020-12-02 12:48:31 +0100

    delete build

--- build/lib/scycle/plots/_themes.py
+++
@@ -1,3 +0,0 @@
-from plotnine import theme_light, theme, element_blank
-
-theme_std = theme_light() + theme(panel_grid=element_blank())
--- build/lib/scycle/preprocess/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._prep_pooling import prep_pooling
-from ._prep_simple import prep_simple
-from ._score_cell_cycle import score_cell_cycle
-# from ._batch_correct import batch_correct
