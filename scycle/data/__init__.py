b4cec2138dd5c5109e68b92d229a36a7d52e0eb1
Clarice <clari.groeneveld@gmail.com>
2021-10-22 11:52:39 +0200

    add Freeman/Tirosh/short

--- scycle/data/__init__.py
+++ scycle/data/__init__.py
@@ -8,6 +8,10 @@ from ._cc_markers import (
     g2m_inhibitory_markers,
     histone_markers,
     cc_genes,
+    g2m_markers_short,
+    g1s_markers_short,
+    G2M_genes_Freeman,
+    G1S_genes_Freeman
 )
 
 from ._get_data import get_data
